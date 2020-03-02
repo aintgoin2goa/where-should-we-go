ec28ef1e002081ad6224aa93e285370172db833a
Paul Wilson <paul.wilson@ktech.com>
2020-03-02 21:28:04 +0000

    fetch data

---
+++ src/server/lib/load-data.ts
@@ -0,0 +1,20 @@
+import * as fetch from "make-fetch-happen";
+
+export interface UserData {
+  name: string;
+  wont_eat: string[];
+  drinks: string[];
+}
+
+const loadData = async <T>(url: string): Promise<T> => {
+  const response = await fetch(url, {
+    retry: 3
+  });
+  if (!response.ok) {
+    throw new Error(`${response.status} ${response.statusText}: ${url}`);
+  }
+
+  return response.json();
+};
+
+export default loadData;
